a007d11d359bc4b93b463c245805a8abfca95338
Breno Barbosa de Oliveira <131599448+Brevex@users.noreply.github.com>
2024-06-02 19:44:30 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,24 +1,41 @@
+#include <WiFi.h>
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT_Client.h>
 #include <DHT.h>
 #include <LiquidCrystal_I2C.h>
 #include <Bounce2.h>
 #include <map>
 
-#define DHTPIN 23      
-#define LDRPIN 34     
-#define BUZZERPIN 12   
-#define BUTTONPIN 14   
+#define WIFI_SSID       "Wokwi-GUEST" // Default Wokwi wifi login
+#define WIFI_PASSWORD   ""
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_USERNAME    "" // Change to your respective login
+#define AIO_KEY         "" // Change to your respective key
+#define AIO_SERVERPORT  1883
+
+#define DHTPIN 23
+#define LDRPIN 34
+#define BUZZERPIN 12
+#define BUTTONPIN 14
 
 #define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal_I2C lcd(0x27, 20, 4);
-Bounce debouncer = Bounce(); 
+Bounce debouncer = Bounce();
 
 float temperature, humidity;
 int luminosity;
 bool buzzerActive = false;
 String alert = "none";
 String previousAlert = "none";
+
+// Change to your Adafruit.io feeds
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+Adafruit_MQTT_Publish temperatureFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temperature");
+Adafruit_MQTT_Publish humidityFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/humidity");
+Adafruit_MQTT_Publish luminosityFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/luminosity");
 
 struct AlertRule 
 {
@@ -59,7 +76,7 @@ void checkAlerts()
     {
       alert = rule.message;
       buzzerActive = true;
-      break; 
+      break;
     }
   }
 }
@@ -76,7 +93,7 @@ void updateLCD()
   if (alert != previousAlert) 
   {
     lcd.setCursor(0, 3);
-    lcd.print("Alert:                "); 
+    lcd.print("Alert:                ");
 
     lcd.setCursor(0, 3);
     lcd.print("Alert: " + alert);
@@ -95,6 +112,39 @@ void handleBuzzer()
   digitalWrite(BUZZERPIN, buzzerActive);
 }
 
+void connectWiFi() 
+{
+  Serial.print("Connecting to WiFi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println(" connected!");
+}
+
+void connectMQTT() 
+{
+  int8_t ret;
+
+  if (mqtt.connected()) { return; }
+
+  Serial.print("Connecting to MQTT... ");
+
+  while ((ret = mqtt.connect()) != 0) 
+  {
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);
+  }
+
+  Serial.println("MQTT Connected!");
+}
+
 void setup() 
 {
   Serial.begin(115200);
@@ -107,6 +157,9 @@ void setup()
 
   debouncer.attach(BUTTONPIN, INPUT_PULLUP);
   debouncer.interval(5);
+
+  connectWiFi();
+  connectMQTT();
 }
 
 void loop() 
@@ -116,4 +169,20 @@ void loop()
   updateLCD();
   handleBuzzer();
   debouncer.update();
+
+  if (WiFi.status() != WL_CONNECTED) 
+  {
+    connectWiFi();
+  }
+
+  connectMQTT();
+
+  if (!mqtt.ping()) 
+  {
+    mqtt.disconnect();
+  }
+
+  temperatureFeed.publish(temperature);
+  humidityFeed.publish(humidity);
+  luminosityFeed.publish(luminosity);
 }
